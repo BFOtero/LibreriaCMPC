2370afebbca95dc3d0851cdc0e5c4bdd03c009cc
Bastián Farías <ba.fotero@gmail.com>
2025-06-17 21:03:24 -0400

    Feat: Se adjunta el sh que ejecuta el seed

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,15 +1,13 @@
-# 1. Inicia el servidor en segundo plano
+
 echo "Iniciando servidor NestJS..."
 node dist/main.js &
 
-# 2. Espera a que el backend esté disponible
 echo "Esperando que el servidor se inicie..."
 until curl --silent --fail http://localhost:3000/api/seed; do
   echo "Esperando backend (retry en 3s)..."
   sleep 3
 done
 
-echo "✅ Seed ejecutado correctamente"
+echo "Seed ejecutado correctamente"
 
-# 3. Mantén el proceso principal corriendo
 wait
